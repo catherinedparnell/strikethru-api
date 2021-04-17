a1b3d470dc856804685d2f60b8bac54939c7d7f4
Catherine Parnell <catherine.d.parnell.22@dartmouth.edu>
2021-04-17 18:54:31 -0400

    cleaning

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,6 @@
 import * as fs from '../services/firestore';
 
 // adds user to firestore
-// req.body = { name: the name of user }
 export const addUser = (req, res) => {
   fs.addUser(req.params.username, req.body.name)
     .then((result) => { res.send(result); })
@@ -11,9 +10,6 @@ export const addUser = (req, res) => {
 };
 
 // add/update preferences in user
-// req.body = { processType: 'word'|'sentence',
-//              filterTypes: ['h'|'t'|'r'|'s'|'a']
-//              chosenFilter: ['words', 'chosen', 'by', 'user']}
 export const updatePreferences = (req, res) => {
   fs.updatePreferences(req.params.username, req.body)
     .then((result) => { res.send(result); })
